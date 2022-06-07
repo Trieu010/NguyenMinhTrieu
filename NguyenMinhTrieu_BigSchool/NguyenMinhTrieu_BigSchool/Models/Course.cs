64a63c5c8994f0ae61e0188982668ea8543e6c19
Trieu010 <91833984+Trieu010@users.noreply.github.com>
2022-06-07 17:19:46 +0700

    dddd

---
+++ NguyenMinhTrieu_BigSchool/NguyenMinhTrieu_BigSchool/Models/Course.cs
@@ -0,0 +1,47 @@
+namespace NguyenMinhTrieu_BigSchool.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+    using System.Linq;
+
+    [Table("Course")]
+    public partial class Course
+    {
+        public List<Category> ListCategory = new List<Category>();
+
+        public List<Category> ListAll()
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            return context.Categories.ToList();
+        }
+        public string Name;
+        public string LectureName;
+
+        public bool IsLogin = false;
+        public bool IsShowGoing = false;
+        public bool IsShowFollow = false;
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string LecturerId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+
+        public DateTime Datetime { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+
+        public virtual Category Category { get; set; }
+        public DateTime DateTime { get; internal set; }
+    }
+}
