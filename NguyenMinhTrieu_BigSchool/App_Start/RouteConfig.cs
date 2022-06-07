a2b5c3f461b147c3f67885ddf60b08d36150990d
Trieu010 <91833984+Trieu010@users.noreply.github.com>
2022-06-07 09:27:17 +0700

    test

--- NguyenMinhTrieu_BigSchool/App_Start/RouteConfig.cs
+++ NguyenMinhTrieu_BigSchool/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace NguyenMinhTrieu_BigSchool
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            /////////đâsdadadad
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
